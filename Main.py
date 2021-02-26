fb32ac2935f35c8d0bad8a125db3320c0a585729
eashvinee <akraihata@gmail.com>
2021-02-26 23:35:19 +0530

    add brand also add item in QComboBox using tuple

--- Main.py
+++ Main.py
@@ -9,6 +9,8 @@ import sqlite3
 from src.AddEmployee import AddEmployee
 from src.AllEmployee import AllEmployee
 
+from src.AddBrand import AddBrand
+from src.AllBrand import AllBrand
 
 class Main(QMainWindow):
     def __init__(self):
@@ -16,11 +18,18 @@ class Main(QMainWindow):
         self.ui=PyQt5.uic.loadUi('ui/Welcome.ui', self)
         #self.setWindowFlags(PyQt5.QtCore.Qt.FramelessWindowHint) 
         self.setWindowIcon(PyQt5.QtGui.QIcon('balloon.svg')) 
+        
         self.addEmployee =None
         self.allEmployee =None
-        
+
+        self.addBrand = None
+        self.allBrand = None
+
         self.ui.actionAdd_New_Employee.triggered.connect(self.action_Add_New_Employee)    
         self.ui.actionAll_Employee.triggered.connect(self.action_All_Employee)    
+
+        self.ui.actionAdd_New_Brand.triggered.connect(self.action_Add_New_Brand)    
+        self.ui.actionAll_Brand.triggered.connect(self.action_All_Brand)    
 
         #print(dir(self.ui.actionAdd_New_Employee.triggered))
         #'actionAdd_New_Brand', 'actionAdd_New_Device', 'actionAdd_New_Employee', 'actionAll_Brand', 'actionAll_Devices', 'actionAll_Employee', 'actionAssign_Device', 'actionBrand', 'actionDevices_History'
@@ -45,6 +54,19 @@ class Main(QMainWindow):
             self.allEmployee= AllEmployee(self) 
         else:
             self.allEmployee.activateWindow()   
+
+    #action_Add_New_Brand
+    def action_Add_New_Brand(self, action):
+        if not isinstance(self.addBrand,  AddBrand):
+            self.addBrand= AddBrand(self) 
+        else:
+            self.addBrand.activateWindow()   
+
+    def action_All_Brand(self, action):
+        if not isinstance(self.allBrand,  AllBrand):
+            self.allBrand= AllBrand(self) 
+        else:
+            self.allBrand.activateWindow()   
 
 
     def developedBy(self):
---
+++ src/AllBrand.py
@@ -0,0 +1,37 @@
+#import PyQt5.QtCore
+from PyQt5.QtCore import *
+import PyQt5.uic
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+import threading
+import time
+import sys
+
+class AllBrand(QWidget):
+    def __init__(self, pself):
+        self.pself=pself
+        super(AllBrand, self).__init__()
+        self.ui=PyQt5.uic.loadUi('ui/AllBrand.ui', self)
+        self.setWindowIcon(PyQt5.QtGui.QIcon('balloon.svg')) 
+        self.setWindowFlags(PyQt5.QtCore.Qt.WindowCloseButtonHint)
+
+        self.view=self.ui.tblBrand
+        #tblBrand
+        rows =  self.pself.connDB.execute("SELECT ID, title, parent FROM tbl_brand").fetchall()
+
+        for row in rows:
+            r = self.view.rowCount()
+            self.view.setRowCount(r + 1)
+            self.view.setItem(r, 0,  QTableWidgetItem(str(row[0])))
+            self.view.setItem(r, 1,  QTableWidgetItem(row[1]))
+            self.view.setItem(r, 2,  QTableWidgetItem(str(row[2])))
+    
+        self.view.resizeColumnsToContents()
+        self.show()
+
+    def closeEvent(self, event):
+        self.pself.allBrand=None
+        self.pself.activateWindow() 
+
+
